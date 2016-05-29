99eeea0a7e05fbce2542e8dd3a7a2e7666d16541
Michael Brookes <mbircohoakeels@me.com>
2016-05-29 20:02:52 +0100

    Base class for handling setup and communication via pwm

--- Lib/PWM/PWMDevice.cpp
+++ Lib/PWM/PWMDevice.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "PWMDevice.h"
+
+namespace abPWM {
+
+    PWMDevice::PWMDevice( int _block, int _pin ) {
+
+        this->SetBlockNum( (BlockNums)_block );
+        this->SetPinNum( (PinNums)_pin );
+
+        if( this->_PWMOverlay.Settings.OverlayLoaded ) {
+            this->_PinOverlay = new PinOverlay( this->PinNum, this->BlockNum );
+
+            if( this->_PinOverlay->Settings.OverlayLoaded ) {
+                //good to go
+            }
+        }
+
+    }
+
+    short PWMDevice::ReadDevice( size_t _BufferSize ) {
+
+    }
+
+    void PWMDevice::InitDevice( ) throw( PWMSetupException& ) {
+
+    }
+
+    int PWMDevice::WriteToDevice( size_t _BufferSize ) throw( PWMSetupException& ) {
+
+    }
+
+    int PWMDevice::OpenDevice( ) throw( PWMSetupException& ) {
+
+    }
+
+    void PWMDevice::SetPinNum( PinNums _pin ) { this->PinNum = _pin; }
+
+    void PWMDevice::SetBlockNum( BlockNums _block ) { this->BlockNum = _block; }
+
+}
--- Lib/PWM/PWMDevice.h
+++ Lib/PWM/PWMDevice.h
@@ -5,10 +5,58 @@
 #ifndef BEAGLEBONE_PWM_PWMDEVICE_H
 #define BEAGLEBONE_PWM_PWMDEVICE_H
 
+#include "../Device/IDevice.h"
+#include "../Exceptions/ExceptionAid.h"
+#include "../Overlays/PWM/PWMOverlay.h"
+#include "../Overlays/PWM/PinOverlay.h"
 
-class PWMDevice {
+namespace abPWM {
 
-};
+    class PWMDevice : public abIDevice::IDevice {
 
+    public:
+
+        PWMDevice( int _block, int _pin );
+
+        short ReadDevice( size_t _BufferSize );
+
+        void InitDevice( ) throw( PWMSetupException& );
+
+        int ConnectToDevice( ) { return 1; }
+
+        int WriteToDevice( size_t _BufferSize ) throw( PWMSetupException& );
+
+        int OpenDevice( ) throw( PWMSetupException& );
+
+        enum BlockNums {
+            P9 = 9, //!< P9 on your BBB | USB facing you, P9 is the pin block on the left hand side.
+            P8 = 8, //!< P8 on your BBB | USB facing you, P8 is the pin block on the right hand side.
+        };
+
+        /**
+         \brief PinNum refers to a exposed PWM Pin on your BBB, this enum can be expanded to include more if required.
+         */
+        enum PinNums {
+            PWM42 = 42, //!< GPIO PWM Pin Number 42
+            PWM22 = 22, //!< GPIO PWM Pin Number 22
+            PWM19 = 19, //!< GPIO PWM Pin Number 19
+            PWM14 = 14, //!< GPIO PWM Pin Number 14
+        };
+
+        BlockNums BlockNum;
+        PinNums PinNum;
+
+    private:
+
+        void SetPinNum( PinNums _pin );
+
+        void SetBlockNum( BlockNums _block );
+
+        PWMOverlay _PWMOverlay;
+        PinOverlay* _PinOverlay;
+
+    };
+
+}
 
 #endif //BEAGLEBONE_PWM_PWMDEVICE_H
