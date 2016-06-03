96c69b6dee52ecaab35c1350cf5e4fa3dd907785
Michael Brookes <mbircohoakeels@me.com>
2016-06-03 16:20:27 +0100

    Adding iDeviceOverlay class to namespace and adding enums for accessing PINS and BLOCKS

--- Lib/Overlays/iDeviceOverlay.cpp
+++ Lib/Overlays/iDeviceOverlay.cpp
@@ -4,45 +4,49 @@
 
 #include "iDeviceOverlay.h"
 
-iDeviceOverlay::iDeviceOverlay( ) {
-}
+namespace abIDevice {
 
-bool iDeviceOverlay::IsLoaded( const char* SearchFile ) {
-#ifdef DEBUG
-    cout << "Searching for : " << SearchFile << endl;
-#endif
-    if( stat( SearchFile, &sb ) == 0 && S_ISREG( sb.st_mode ) ) {
-#ifdef DEBUG
-        cout << "-- Found it..." << endl;
-#endif
-        return true;
+    iDeviceOverlay::iDeviceOverlay( ) {
     }
-#ifdef DEBUG
-    cout << "-- File not found..." << endl;
-#endif
-    return false;
-}
 
-bool iDeviceOverlay::Load( const char* Overlay ) {
-    try {
+    bool iDeviceOverlay::IsLoaded( const char *SearchFile ) {
 #ifdef DEBUG
-        cout << "-- Overlay not loaded yet, attempting to load : " << Overlay << endl;
+        cout << "Searching for : " << SearchFile << endl;
 #endif
-        this->FileHandle = open( SLOTS_DIR, O_WRONLY );
-        if( write( this->FileHandle, Overlay, 1024 ) > 0 ) {
-            close( this->FileHandle );
+        if( stat( SearchFile, &sb ) == 0 ) {
 #ifdef DEBUG
-            cout << "--- Overlay loaded successfully." << endl;
+            cout << "-- Found it..." << endl;
 #endif
             return true;
         }
-        else {
 #ifdef DEBUG
-            cout << "--- Failed to load overlay." << endl;
+        cout << "-- File not found..." << endl;
 #endif
-            return false;
-        }
-    } catch( exception& e ) {
-        cerr << "Exception : " << e.what() << endl;
+        return false;
     }
+
+    bool iDeviceOverlay::Load( const char *Overlay ) {
+        try {
+#ifdef DEBUG
+            cout << "-- Overlay not loaded yet, attempting to load : " << Overlay << endl;
+#endif
+            this->FileHandle = open( SLOTS_DIR, O_WRONLY );
+            if( write( this->FileHandle, Overlay, 1024 ) > 0 ) {
+                close( this->FileHandle );
+#ifdef DEBUG
+                cout << "--- Overlay loaded successfully." << endl;
+#endif
+                return true;
+            }
+            else {
+#ifdef DEBUG
+                cout << "--- Failed to load overlay." << endl;
+#endif
+                return false;
+            }
+        } catch( exception &e ) {
+            cerr << "Exception : " << e.what( ) << endl;
+        }
+    }
+
 }
--- Lib/Overlays/iDeviceOverlay.h
+++ Lib/Overlays/iDeviceOverlay.h
@@ -18,27 +18,43 @@
 #define ANALOGDEVICE_OVERLAYS_H
 
 #define RETRIES 99
-#define DEBUG
+//#define DEBUG
 
 #define SLOTS_DIR "/sys/devices/bone_capemgr.9/slots" //!< Full system path to SLOTS, used to export device tree overlays.
 
 using namespace std;
 
-class iDeviceOverlay {
-public:
-    iDeviceOverlay( );
+namespace abIDevice {
+    class iDeviceOverlay {
+    public:
+        iDeviceOverlay( );
 
-    bool IsLoaded( const char* SearchFile );
+        bool IsLoaded( const char *SearchFile );
 
-    bool Load( const char* Overlay );
+        bool Load( const char *Overlay );
 
-    int FileHandle;
+        int FileHandle;
 
-    char ErrMessage[ 1024 ];
+        char ErrMessage[1024];
 
-    struct stat sb;
+        struct stat sb;
 
-};
+        enum PinBlocks {
+            BLOCK_P9 = 9, //!< P9 on your BBB | USB facing you, P9 is the pin block on the left hand side.
+            BLOCK_P8 = 8, //!< P8 on your BBB | USB facing you, P8 is the pin block on the right hand side.
+        };
 
+        /**
+         \brief PinNum refers to a exposed PWM Pin on your BBB, this enum can be expanded to include more if required.
+         */
+        enum PWMPins {
+            PIN_42 = 42, //!< GPIO PWM Pin Number 42
+            PIN_22 = 22, //!< GPIO PWM Pin Number 22
+            PIN_19 = 19, //!< GPIO PWM Pin Number 19
+            PIN_14 = 14, //!< GPIO PWM Pin Number 14
+        };
+
+    };
+}
 
 #endif //ANALOGDEVICE_OVERLAYS_H
